051415f029729bc4221acb85bc0c8fec8a638ca1
Sakshi <70653660+Sakshi887220@users.noreply.github.com>
2022-06-25 19:32:21 +0530

    Update height.py

--- Normal-Distribution-master/height.py
+++ Normal-Distribution-master/height.py
@@ -1,7 +1,1 @@
-import plotly.figure_factory as ff
-import pandas as pd
-import csv
 
-df = pd.read_csv("data.csv")
-fig = ff.create_distplot([df["Height(Inches)"].tolist()], ["Height"], show_hist=False)
-fig.show()
